58c9124a2cb805c0c40006ee3e6865e23ca32e86
meepoorkeepo <djfaycel06@gmail.com>
2025-12-24 09:07:21 +0100

    starting the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <header>
+        TS ARIZONI
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All rigths reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
