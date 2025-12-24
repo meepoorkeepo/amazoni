15d29cb70aa6a96a77f3a85e12e362c14906b798
meepoorkeepo <djfaycel06@gmail.com>
2025-12-24 12:49:54 +0100

    added products and rendered them

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { sampleProducts } from "./data"
+import './App.css'
+
 function App() {
   return (
     <div>
       <header>
-        TS ARIZONI
+        AMAZONI
       </header>
       <main>
-
+        <ul>
+          {sampleProducts.map((product)=>(
+            <div key={product.slug}>
+              <img className="img" src={product.image} alt={product.slug} />
+              <h2>{product.name}</h2>
+              <p>€{product.price}</p>
+            </div>
+          ))}
+        </ul>
       </main>
       <footer>
         All rigths reserved
